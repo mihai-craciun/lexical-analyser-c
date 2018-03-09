8c5de877292304d93bd8e0b7735d99eee0f57d64
Mihai <mihaicraciun96@gmail.com>
2018-03-09 17:43:25 +0200

    initial commit, added states, c keywords but not yet parses C

---
+++ analizor_lexical_c.py
@@ -0,0 +1,216 @@
+from transitions import *
+from time import sleep
+from sys import exit
+from collections import defaultdict
+from pprint import pprint
+
+
+class Tokenizer():
+    KEYWORDS = ['auto', 'int', 'const', 'short', 'break', 'long', 'continue', 'signed', 'double', 'struct','float', 'unsigned', 'else', 'switch', 'for', 'void', 'case', 'register', 'default', 'sizeof', 'char', 'return', 'do', 'static', 'enum', 'typedef', 'goto', 'volatile', 'extern', 'union', 'if', 'while']
+
+    def __init__(self, filepath):
+        with open(filepath) as stream:
+            self.source_code = stream.read()
+        self.position = 0
+        self.tabela = defaultdict(list)
+
+    def gettoken(self):
+        dfa = Dfa(self.source_code[self.position:])
+
+        # import ipdb; ipdb.set_trace()
+        token_val, token_type, consumed_chars = dfa.run()
+        self.position += consumed_chars
+
+        MAPPER = {
+            'CHARACTER_CAN_BE_FOLLOWED_BY_EQUAL': 'OPERATOR',
+            'STRING_SIMPLE_QUOTES_END': 'STRING',
+            'CHARACTER': 'OPERATOR',
+            'GROUP_CHARACTER': 'GROUP_OPERATOR',
+        }
+
+        if token_type in MAPPER:
+            token_type = MAPPER[token_type]
+
+        if token_type == 'IDENT':
+            if token_val in self.KEYWORDS:
+                token_type = 'KEYWORD'
+        elif token_type == 'NEWLINE':
+            token_val = len(token_val) - 1
+            token_type = 'INDENTATION'
+        elif token_type in ['COMMENT', 'SPACE']:
+            return self.gettoken()
+
+        self.tabela[(token_type, token_val)].append(self.position - consumed_chars)
+        return self.position - consumed_chars
+
+
+class Dfa():
+
+    STATES_LIST = [
+        'INITIAL',
+        'IDENT',
+        'SPACE',
+        'NEWLINE',
+        'CHARACTER',
+        'NUMBER',
+        'COMMENT',
+        'STRING_SIMPLE_QUOTES',
+        'STRING_DOUBLE_QUOTES',
+        'STRING_SIMPLE_QUOTES_END',
+        'STRING_DOUBLE_QUOTES_END',
+        'NEGATIVE_SIGN',
+        'FLOAT_NUMBER',
+        'ESCAPING',
+        'CHARACTER_ESCAPED',
+        'CHARACTER_CAN_BE_FOLLOWED_BY_EQUAL',
+        'GROUP_CHARACTER',
+        'EXPONENT',
+        'ZERO',
+        'HEXA',
+        'END',
+        'ERROR',
+    ]
+
+    STATES = {}
+    for index, state in enumerate(STATES_LIST):
+        STATES[state] = index
+
+    NONPRODUCTING_STATES = [
+        STATES['ESCAPING'],
+        STATES['CHARACTER_ESCAPED'],
+    ]
+
+    def __init__(self, input_list):
+        self.state = 0
+        self.TRANSITIONS = {
+            self.STATES['INITIAL']: [
+                [is_negative_sign, self.STATES['NEGATIVE_SIGN']],
+                [lambda x: x == '0', self.STATES['ZERO']],
+                [is_allowed_first_char_for_id, self.STATES['IDENT']],
+                [is_space, self.STATES['SPACE']],
+                [is_newline, self.STATES['NEWLINE']],
+                [is_digit, self.STATES['NUMBER']],
+                [is_character_can_be_followed_by_equal, self.STATES['CHARACTER_CAN_BE_FOLLOWED_BY_EQUAL']],
+                [is_character, self.STATES['CHARACTER']],
+                [is_hash, self.STATES['COMMENT']],
+                [is_simple_quote, self.STATES['STRING_SIMPLE_QUOTES']],
+                [is_double_quote, self.STATES['STRING_DOUBLE_QUOTES']],
+                [anything, self.STATES['ERROR']],
+            ],
+            self.STATES['IDENT']: [
+                [is_allowed_char_for_id, self.STATES['IDENT']],
+                [is_not_allowed_char_for_id, self.STATES['END']],
+            ],
+            self.STATES['SPACE']: [
+                [is_space, self.STATES['SPACE']],
+                [anything, self.STATES['END']],
+            ],
+            self.STATES['NEWLINE']: [
+                [is_tab, self.STATES['NEWLINE']],
+                [anything, self.STATES['END']],
+            ],
+            self.STATES['CHARACTER']: [
+                [anything, self.STATES['END']],
+            ],
+            self.STATES['NUMBER']: [
+                [is_digit, self.STATES['NUMBER']],
+                [is_point, self.STATES['FLOAT_NUMBER']],
+                [is_e, self.STATES['EXPONENT']],
+                [anything, self.STATES['END']],
+            ],
+            self.STATES['FLOAT_NUMBER']: [
+                [is_digit, self.STATES['NUMBER']],
+                [anything, self.STATES['END']],
+            ],
+            self.STATES['COMMENT']: [
+                [is_newline, self.STATES['END']],
+                [anything, self.STATES['COMMENT']],
+            ],
+            self.STATES['STRING_SIMPLE_QUOTES']: [
+                [is_escape, self.STATES['ESCAPING']],
+                [is_simple_quote, self.STATES['STRING_SIMPLE_QUOTES_END']],
+                [is_newline, self.STATES['ERROR']],
+                [anything, self.STATES['STRING_SIMPLE_QUOTES']],
+            ],
+            self.STATES['STRING_SIMPLE_QUOTES_END']: [
+                [anything, self.STATES['END']]
+            ],
+            self.STATES['NEGATIVE_SIGN']: [
+                [is_digit, self.STATES['NUMBER']],
+                [anything, self.STATES['CHARACTER']],
+            ],
+            self.STATES['ESCAPING']: [
+                [is_escape, self.STATES['STRING_SIMPLE_QUOTES']],
+                [is_simple_quote, self.STATES['STRING_SIMPLE_QUOTES']],
+                [anything, self.STATES['CHARACTER_ESCAPED']],
+            ],
+            self.STATES['CHARACTER_ESCAPED']: [
+                [anything, self.STATES['STRING_SIMPLE_QUOTES']],
+            ],
+            self.STATES['CHARACTER_CAN_BE_FOLLOWED_BY_EQUAL']: [
+                [is_equal, self.STATES['GROUP_CHARACTER']],
+                [anything, self.STATES['END']],
+            ],
+            self.STATES['GROUP_CHARACTER']: [
+                [anything, self.STATES['END']],
+            ],
+            self.STATES['EXPONENT']: [
+                [is_negative_sign, self.STATES['NEGATIVE_SIGN']],
+                [is_digit, self.STATES['NUMBER']],
+                [anything, self.STATES['END']],
+            ],
+            self.STATES['ZERO']: [
+                [lambda x: x == 'x', self.STATES['HEXA']],
+                [is_digit, self.STATES['NUMBER']],
+                [anything, self.STATES['END']],
+            ],
+            self.STATES['HEXA']: [
+                [is_hexa_char, self.STATES['HEXA']],
+                [anything, self.STATES['END']],
+            ],
+        }
+        self.position = 0
+        self.input = input_list
+        self.states = []
+        self.output = []
+
+    def consume(self):
+        if self.position == len(self.input):
+            pprint(tokenizer.tabela)
+            exit(0)
+
+        state = self.TRANSITIONS[self.state]
+        for transition_function, transition_state in state:
+            if transition_function(self.input[self.position]):
+                self.states.append(self.state)
+                if transition_state not in self.NONPRODUCTING_STATES:
+                    self.output.append(self.input[self.position])
+
+                self.state = transition_state
+                self.position += 1
+                return
+        raise Exception('No transition function')
+
+    def run(self):
+        # if self.STATES['ERROR'] == self.state:
+        # 	raise Exception('Error')
+        while self.state != self.STATES['END']:
+            self.consume()
+
+        for key in self.STATES:
+            if self.STATES[key] == max(self.states):
+                return [''.join(self.output[:-1]), key, self.position-1]
+
+
+tokenizer = Tokenizer('./test.c')
+while True:
+    try:
+        pozitie_de_caracter = tokenizer.gettoken()
+        # sleep(0.1)
+    except Exception:
+        print("Am ajuns la o eroare")
+        exit(0)
+
+    for key in tokenizer.tabela:
+        if pozitie_de_caracter in tokenizer.tabela[key]:
+            print("{0} - {1}".format(*key))
---
+++ transitions.py
@@ -0,0 +1,65 @@
+def is_allowed_first_char_for_id(c):
+    return is_letter(c) or c in ['_']
+
+def is_allowed_char_for_id(c):
+    return is_allowed_first_char_for_id(c) or is_digit(c)
+
+def is_not_allowed_char_for_id(c):
+    return not is_allowed_char_for_id(c)
+
+def is_letter(c):
+    return 'a' <= c <= 'z' or 'A' <= c <= 'Z'
+
+def is_digit(c):
+    return c.isdigit()
+
+def anything(c):
+    return True
+
+def is_space(c):
+    return ' ' == c
+
+def is_newline(c):
+    return '\n' == c
+
+def is_tab(c):
+    return '\t' == c
+
+def is_character(c):
+    return c in ['*', '+', '-', '(', ')', '[', ']', '{', '}', ':', '=', '.', ',', '>', '<', '!', ';']
+
+def is_hash(c):
+    return c == '#'
+
+def is_simple_quote(c):
+    return c == "'" or c =='"'
+
+def is_single_quote(c):
+    return c == "'"
+
+def is_double_quote(c):
+    return c == '"'
+
+def is_negative_sign(c):
+    return c == '-'
+
+def is_point(c):
+    return c == '.'
+
+def is_escape(c):
+    return c == '\\'
+
+def is_slash(c):
+    return c =='/'
+
+def is_character_can_be_followed_by_equal(c):
+    return c in ['+', '-', '*', '/', '<', '>']
+
+def is_equal(c):
+    return c == '='
+
+def is_e(c):
+    return c == 'e'
+
+def is_hexa_char(c):
+    return is_digit(c) or 'a' <= c <= 'f'
