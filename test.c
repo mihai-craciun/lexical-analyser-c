7e7c7b6873b798b7bc760d110d897a5eaf313bcb
Mihai <mihaicraciun96@gmail.com>
2018-03-13 17:12:33 +0200

    added dragulici`s stuff

---
+++ test.c
@@ -0,0 +1,23 @@
+int NULL = 0;
+int f(int* x, char** v) {
+    return 3;
+}
+void g() {
+    f((void*)NULL,(void*)NULL);
+}
+int main(int argc, char** argv) {
+    // asta-i un comment
+    // asta-i alt comment
+    /* asta-i multi line
+    comment
+    smecher * ** *//* inca unul * ** */
+    /*****/
+    int a = 3;
+    float d = 3.13e0L;
+    char* x="this is my \" escaped string";
+    char c='\xff';
+    int b = 3;
+    double db = 10.e+2;
+    int dbb = 0xfffcedf;
+    g();
+}
